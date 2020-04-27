6145f855944b0d2e4eb9565c7b922f7d6785cb68
nanaji0133 <nanajisanka5357@gmail.com>
2020-04-27 19:43:04 -0400

    arrow functions

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 "use strict";
 
-var i = "nanaji"
-function test() {
-    let i = "sanka";
+
+// arrow function or annonumous function
+
+// cont varfn = function () {
+//     return "arow function"
+// }
+
+
+const varfn = () => "arrow function";
+
+const varfn1 = (num1, num2) => num1+num2;
+
+
+console.log(varfn)
+console.log(varfn1(3,5))
+
+
+const myArr = [1, 2, -3, -4, 5];
+
+const squareList = (arr) => {
+    const squared = arr.filter(num => Number.isInteger(num) && num>0).map(x => x*x)
+    return squared
 }
 
+const squareOutput = squareList(myArr)
+console.log(squareOutput)
 
-console.log(test())
-console.log(i)
 
+const increment = (function() {
+    return function increment (num, value=1) {
+        return num + value
+    };
+})();
+
+console.log(increment(5,2))
+console.log(increment(5))
