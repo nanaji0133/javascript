00f7b5325cbbf9b4f77e63b61034ef8f46b8b023
nanaji0133 <nanajisanka5357@gmail.com>
2020-04-26 20:08:00 -0400

    ternary

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-myArray = [
-    {
-        "name": "nanaji",
-        "surname": "sanka",
-        "age": 22
-    },
+// ternary operation 
+// check +,- or 0
 
-    {
-        "name": "shree",
-        "surname": "ranga",
-        "age": 31
-    },
-];
-
-
-function propValue(fname, prop) {
-    for (var i = 0; i < myArray.length; i++) {
-        if (myArray[i]["name"] === fname) {
-            if (myArray[i][prop]) {
-                return myArray[i][prop]
-            } else {
-                return "no such prop existing"
-            }
-        }
-    }
-    return "no fname"
+function ternary(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero"
 }
 
-console.log(propValue("nanajioo", "age1"))
+console.log(ternary(-10))
