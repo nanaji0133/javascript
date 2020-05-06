2a9bbdcf6003071b3675727e6dd9b41a988272bd
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-06 19:01:46 -0400

    MDN event task1

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-function displayMsg(msgText, msgType)
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", btnMsg)
+
+
+function btnMsg(e)
 {
-    let html = document.querySelector("html");
-    let pannel = document.createElement("div");
-    html.appendChild(pannel)
-    let msg = document.createElement("p");
-    msg.textContent = msgText;
-    pannel.appendChild(msg)
-    let msgBtn = document.createElement("button")
-    msgBtn.textContent = "X"
-    pannel.appendChild(msgBtn)
-
-    if (msgType === "warning")
+    if (e.target.textContent === "machine is off")
     {
-        msg.style.backgroundImage = "url(img/warning.png)";
-        pannel.style.backgroundColor = "red";
-    } else if (msgType === "chat")
+        e.target.textContent = "machine is on"
+    } else
     {
-        msg.style.backgroundImage = "url(img/chat.png)";
-
+        e.target.textContent = "machine is off"
     }
-
-    msgBtn.onclick = function ()
-    {
-        pannel.parentNode.removeChild(pannel)
-    }
-}
-
-let button = document.querySelector("button");
-
-button.onclick = function ()
-{
-    displayMsg("this is msg", "chat")
-}
+};
 
