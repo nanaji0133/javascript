3a5d377874f3321a5e2c8bbc37f5233fb39a3aff
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-07 16:29:23 -0400

    OOJS MDN practice of fnconst to create objs

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const btn = document.querySelector("button")
-
-btn.addEventListener("click", btnMsg)
-
-
-function btnMsg(e)
+function Person(first, last, age, gender, interest)
 {
-    if (e.target.textContent === "machine is off")
-    {
-        e.target.textContent = "machine is on"
-    } else
-    {
-        e.target.textContent = "machine is off"
+    this.name = {
+        first: first,
+        last: last,
     }
-};
+    this.age = age;
+    this.gender = gender;
+    this.interest = interest;
+    let bioStr = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ';
+    for (let i = 0; i < this.interest.length; i++)
+    {
+        if (i === this.interest.length - 1)
+        {
+            bioStr += " and " + this.interest[i] + "."
+        } else
+        {
+            bioStr += this.interest[i] + ", "
+        }
+
+    }
+    if (this.gender === "female")
+    {
+        bioStr = bioStr.replace("He", "She")
+    }
+    this.bio = function ()
+    {
+        alert(bioStr);
+    };
+    this.greeting = function ()
+    {
+        alert("my name is " + this.name.first + ".")
+    }
+}
 
